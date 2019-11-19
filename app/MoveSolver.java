64677deda00d1db3ccb9962597368f49fbec95c5
khoitd1997 <khoidinhtrinh@gmail.com>
2019-11-19 13:33:05 -0800

    added base move preprocessor

--- app/MoveSolver.java
+++ app/MoveSolver.java
@@ -56,9 +56,9 @@ class UserMove {
     public MoveType moveType;
     public Point3Dim pt;
 
-    public UserMove(MoveType mType) {
+    public UserMove(MoveType mType, Point3Dim pt) {
         this.moveType = mType;
-        // this.pt = pt;
+        this.pt = pt;
     }
 }
 
@@ -131,55 +131,86 @@ class Location {
         }
     }
 
-    public void update(UserMove move) {
+    // convert raw moves into moves that make sense
+    // for example, convert two close left turn to 1 u left turn
+    public void preprocessMove(List<UserMove> userMoves) {
+        List<UserMove> removeList = new ArrayList<>();
+        for (int i = 0; i < userMoves.size() - 1; ++i) {
+            UserMove currMove = userMoves.get(i);
+            UserMove nextMove = userMoves.get(i + 1);
+            if (Math.abs(currMove.pt.y - nextMove.pt.y) < 5) {
+                boolean doRemove = false;
+                if (currMove.moveType == nextMove.moveType) {
+                    if (currMove.moveType == MoveType.LEFT) {
+                        currMove.moveType = MoveType.U_TURN_LEFT;
+                        doRemove = true;
+                    } else if (currMove.moveType == MoveType.RIGHT) {
+                        currMove.moveType = MoveType.U_TURN_RIGHT;
+                        doRemove = true;
+                    }
+                } else if ((Math.abs(currMove.pt.y - nextMove.pt.y) < 4) && currMove.moveType == MoveType.LEFT
+                        && nextMove.moveType == MoveType.RIGHT) {
+                    currMove.moveType = MoveType.UP_SHIFT_LEFT;
+                    doRemove = true;
+                }
+                if (doRemove) {
+                    ++i;
+                    removeList.add(nextMove);
+                }
+            }
+        }
+        userMoves.removeAll(removeList);
+    }
+
+    public void update(MoveType moveType) {
         if (currHeading == HeadingType.TOP) {
-            if (isMoveToRight(move.moveType)) {
+            if (isMoveToRight(moveType)) {
                 sector += 1;
-            } else if (isMoveToLeft(move.moveType)) {
+            } else if (isMoveToLeft(moveType)) {
                 sector -= 1;
-            } else if (move.moveType == MoveType.UP_SHIFT_LEFT) {
+            } else if (moveType == MoveType.UP_SHIFT_LEFT) {
                 sector -= 1;
             } else {
-                throw new RuntimeException("top fell through " + move.moveType + " " + currHeading);
+                throw new RuntimeException("top fell through " + moveType + " " + currHeading);
             }
         } else if (currHeading == HeadingType.BOTTOM) {
-            if (isMoveToLeft(move.moveType)) {
+            if (isMoveToLeft(moveType)) {
                 sector += 1;
-            } else if (isMoveToRight(move.moveType)) {
+            } else if (isMoveToRight(moveType)) {
                 sector -= 1;
             } else {
-                throw new RuntimeException("bottom fell through " + move.moveType + " " + currHeading);
+                throw new RuntimeException("bottom fell through " + moveType + " " + currHeading);
             }
         } else if (currHeading == HeadingType.LEFT) {
-            if (move.moveType == MoveType.LEFT || move.moveType == MoveType.RIGHT) {
+            if (moveType == MoveType.LEFT || moveType == MoveType.RIGHT) {
                 sector = 0;
             } else {
-                throw new RuntimeException("left fell through " + move.moveType + " " + currHeading);
+                throw new RuntimeException("left fell through " + moveType + " " + currHeading);
             }
         } else if (currHeading == HeadingType.RIGHT) {
-            if (move.moveType == MoveType.RIGHT) {
+            if (moveType == MoveType.RIGHT) {
                 sector += 1;
-            } else if (move.moveType == MoveType.LEFT) {
+            } else if (moveType == MoveType.LEFT) {
                 sector += 1;
             } else {
-                throw new RuntimeException("right fell through " + move.moveType + " " + currHeading);
+                throw new RuntimeException("right fell through " + moveType + " " + currHeading);
             }
         }
         if (sector == 0) {
-            if (isMoveToLeft(move.moveType)) {
+            if (isMoveToLeft(moveType)) {
                 goUpFloor();
-            } else if (isMoveToRight(move.moveType)) {
+            } else if (isMoveToRight(moveType)) {
                 goDownFloor();
             } else {
-                throw new RuntimeException("sector fell through " + move.moveType + " " + currHeading);
+                throw new RuntimeException("sector fell through " + moveType + " " + currHeading);
             }
         }
-        updateHeading(move.moveType);
+        updateHeading(moveType);
         if (floor > maxFloor || floor < minFloor) {
-            throw new RuntimeException("floor invalid " + floor + " " + move.moveType + " " + currHeading);
+            throw new RuntimeException("floor invalid " + floor + " " + moveType + " " + currHeading);
         }
         if (sector > maxSector || sector < minSector) {
-            throw new RuntimeException("sector invalid " + sector + " " + move.moveType + " " + currHeading);
+            throw new RuntimeException("sector invalid " + sector + " " + moveType + " " + currHeading);
         }
         System.out.println("floor " + floor + " sector " + sector + " heading " + currHeading);
     }
@@ -187,16 +218,31 @@ class Location {
 
 public class MoveSolver {
     public static void main(String[] args) {
+        // {
+        // Location location = new Location(2, 2, 0, 2, 3, 0);
+        // location.update(MoveType.UP_SHIFT_LEFT);
+        // location.update(MoveType.U_TURN_LEFT);
+        // location.update(MoveType.LEFT);
+        // location.update(MoveType.LEFT);
+        // location.update(MoveType.U_TURN_LEFT);
+        // location.update(MoveType.U_TURN_RIGHT);
+        // location.update(MoveType.U_TURN_RIGHT);
+        // location.update(MoveType.U_TURN_RIGHT);
+        // }
+
         {
             Location location = new Location(2, 2, 0, 2, 3, 0);
-            location.update(new UserMove(MoveType.UP_SHIFT_LEFT));
-            location.update(new UserMove(MoveType.U_TURN_LEFT));
-            location.update(new UserMove(MoveType.LEFT));
-            location.update(new UserMove(MoveType.LEFT));
-            location.update(new UserMove(MoveType.U_TURN_LEFT));
-            location.update(new UserMove(MoveType.U_TURN_RIGHT));
-            location.update(new UserMove(MoveType.U_TURN_RIGHT));
-            location.update(new UserMove(MoveType.U_TURN_RIGHT));
+            List<UserMove> testMoves = new ArrayList<>();
+            testMoves.add(new UserMove(MoveType.LEFT, new Point3Dim(0, 0, 0)));
+            testMoves.add(new UserMove(MoveType.RIGHT, new Point3Dim(2, 2, 0)));
+            testMoves.add(new UserMove(MoveType.LEFT, new Point3Dim(10, 5, 2)));
+            testMoves.add(new UserMove(MoveType.LEFT, new Point3Dim(14, 9, 2)));
+            testMoves.add(new UserMove(MoveType.LEFT, new Point3Dim(30, 40, 20)));
+
+            location.preprocessMove(testMoves);
+            for (UserMove testMove : testMoves) {
+                System.out.println(testMove.moveType);
+            }
         }
     }
 }
